420230ee4457d291a37e70676e68b10cf8b3227f
Fernando <fernandollucas18@gmail.com>
2021-04-14 21:14:42 -0300

    Foram implementados metodos que encontram o dino

---
+++ Main.py
@@ -0,0 +1,57 @@
+""" bibliotecas nativas """
+import time
+import pyautogui
+from PIL import ImageGrab
+
+""" classes exclusivas """
+from Scanner import scanUntil
+
+# DEFINIÇÕES DE COR
+#Esta é a cor do Dino, também usada por Obstáculos.
+COLOR_DINOSAUR = (83, 83, 83)
+DARK_COLOR_DINO = (172, 172, 172)
+
+screen = ImageGrab.grab()
+
+#x = scanUntil([20, 80], [0, 15], COLOR_DINOSAUR, False, 500 / 15)
+#print(x)
+
+def findGamePosition():
+    pos = None
+    dinoPos = None
+    skipXFast = 15
+
+    for x in range(20, screen.width, skipXFast):
+        dinoPos = scanUntil([x, 80], #Start position
+                            [0, skipXFast], #Skip pixels
+                            COLOR_DINOSAUR, #Searching Color
+                            False, #Normal mode (not inverse)
+                            500 / skipXFast) #Iteration limit
+        if(dinoPos):
+            return dinoPos
+            break
+      
+
+x = findGamePosition()
+pyautogui.moveTo(x)
+print('Achou!')
+
+""" Código Velho: """
+if (1 > 2):
+    screen = ImageGrab.grab()
+    screen.save(r'print.png')
+
+    print('Fim')
+
+    """Localiza o Dinossauro"""
+    x = pyautogui.locateOnScreen('dino.png')
+    y = pyautogui.locateCenterOnScreen('dino.png')
+    pyautogui.moveTo(y)
+
+    print('Tipo: ', type(x))
+    print('x: ', x)
+    print('Centro: ', y)
+
+    """ Inicia o Jogo """
+    time.sleep(5)
+    pyautogui.press("up")
---
+++ Scanner.py
@@ -0,0 +1,38 @@
+from PIL import ImageGrab
+import pyautogui
+
+
+screen = ImageGrab.grab()
+
+# Indexes
+X = 0
+Y = 1
+
+def scanUntil(start, delta, matchColor, inverted, iterLimit):
+    color = None
+    current = None 
+    iterations = 0
+    pos = [start[X], start[Y]]
+    
+    while(not isOutOfBound(pos)):
+        #pyautogui.moveTo(pos) #Move o Mouse
+        color = screen.getpixel((pos[X], pos[Y]))
+
+        if(color == matchColor):
+            return pos
+
+        pos[X] += delta[X]
+        pos[Y] += delta[Y]
+        iterations += 1
+
+        if(iterations > iterLimit):
+            return None
+
+    return None
+
+# Check if the given position is outside the Screen
+# Verifique se a posição dada está fora da tela
+def isOutOfBound(pos):
+    if(pos[X] < 0 or pos[Y] < 0 or pos[X] >= screen.width or pos[Y] >= screen.height):
+       return True
+    return False
